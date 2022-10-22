0468a0ba9c402d50ab9dbcde4a23dba231ac9d3d
Daniel Leite <daniel.leite@comcarde.com>
2022-10-22 14:33:32 +0100

    updates

--- js/app.80a32226.js
+++ js/app.80a32226.js
@@ -1460,19 +1460,19 @@
       },
       26: function (e, t, i) {
         var s = {
-          '/background-image-1.jpg': 970,
-          '/profile-image.jpg': 760,
-          '/project_1.jpg': 195,
-          '/project_10.jpg': 437,
-          '/project_11.jpg': 68,
-          '/project_2.jpg': 455,
-          '/project_3.jpg': 140,
-          '/project_4.jpg': 502,
-          '/project_5.jpg': 966,
-          '/project_6.jpg': 275,
-          '/project_7.jpg': 194,
-          '/project_8.jpg': 841,
-          '/project_9.jpg': 394,
+          './background-image-1.jpg': 970,
+          './profile-image.jpg': 760,
+          './project_1.jpg': 195,
+          './project_10.jpg': 437,
+          './project_11.jpg': 68,
+          './project_2.jpg': 455,
+          './project_3.jpg': 140,
+          './project_4.jpg': 502,
+          './project_5.jpg': 966,
+          './project_6.jpg': 275,
+          './project_7.jpg': 194,
+          './project_8.jpg': 841,
+          './project_9.jpg': 394,
         };
         function a(e) {
           var t = n(e);
